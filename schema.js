a1d7bd9d3da983e5e894034ed0c3064528248594
chatesakshi10 <chatesakshi10@gmail.com>
2026-02-12 01:15:00 +0530

    Changes 2

--- schema.js
+++ schema.js
@@ -7,5 +7,9 @@ module.exports.listingSchema = Joi.object({
         price: Joi.number().min(0).required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
+        image: Joi.object({           // optional
+            filename: Joi.string().allow(""), // allow empty string
+            url: Joi.string().uri().allow("") // allow empty string
+        }).optional()
     }).required(),
 });
